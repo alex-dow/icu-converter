850a71bbe119a740adb39eb68162b68f3738d208
v0idnull <v0idnull@gmail.com>
2015-06-06 15:43:40 -0400

    Cleaned up the code and fixed some bugs, especially related to array handling

--- src/writers/properties.js
+++ src/writers/properties.js
@@ -1,9 +1,73 @@
+'use strict';
+
 var _ = require('lodash');
 var path = require('path');
 var mkdirp = require('mkdirp');
 var fs = require('fs');
 
-'use strict';
+function processString(str) {
+  str = str.replace(/\n/g, '\\\n');
+  str = str.replace(/\r/g, '\\\r');
+  return str;
+}
+
+function processArray(arr, key) {
+
+  var newArr = [];
+
+  arr.forEach(function(obj, idx) {
+    var propObj = {};
+    var propKey = key + '.' + idx;
+    if (_.isString(obj)) {
+      propObj[propKey] = processString(obj);
+    } else {
+      // FIXME Better solutiosn to handling complex types in arrays?
+      propObj[propKey] = JSON.stringify(obj);
+    }
+
+    newArr.push(propObj);
+  });
+
+  return newArr;
+
+} 
+
+function flatten(obj, parentKey, processedObject) {
+
+  for (var property in obj) {
+    if (obj.hasOwnProperty(property)) {
+      if (_.isObject(obj[property]) && !_.isArray(obj[property]) && !_.isString(obj[property])) {
+        flatten(obj[property], parentKey + '.' + property, processedObject);
+
+      } else if (_.isString(obj[property])) {
+
+        var propObj = {};
+        propObj[parentKey + '.' + property] = processString(obj[property]);
+        processedObject.push(propObj);
+
+      } else if (_.isArray(obj[property])) {
+        var arr = processArray(obj[property], parentKey + '.' + property);
+        processedObject.push.apply(processedObject, arr);
+
+      } else {
+        console.log('unsupported data type, dude that blows. I don"t even know how we go there?');
+      }
+    }
+  }
+}
+
+function processObject(obj) {
+
+  var processedObject = [];
+
+  for (var k in obj) {
+    if (obj.hasOwnProperty(k)) {
+      flatten(obj[k], k, processedObject);
+    }
+  }
+
+  return processedObject;
+}
 
 var Writer = function(obj, inputFile, outputDir, argOptions) {
 
@@ -32,63 +96,7 @@ var Writer = function(obj, inputFile, outputDir, argOptions) {
   console.log("Writing to " + outputFile);
   fs.writeFileSync(outputFile, propertiesFile);
 
-}
+};
 
-function processString(str) {
-  str = str.replace(/\n/g, '\\\n');
-  str = str.replace(/\r/g, '\\\r');
-  return str;
-}
-
-function flatten(obj, parentKey, processedObject) {
-  for (var property in obj) {
-    if (obj.hasOwnProperty(property)) {
-      if (_.isObject(obj[property]) && !_.isArray(obj[property]) && !_.isString(obj[property])) {
-
-        flatten(obj[property], parentKey + '.' + property, processedObject);
-
-      } else if (_.isString(obj[property])) {
-
-        var propObj = {};
-
-        propObj[parentKey + '.' + property] = processString(obj[property]);
-        processedObject.push(propObj);
-
-      } else if (_.isArray(obj[property])) {
-        for (var i = 0; i < obj[property].length; i++) {
-
-          propObj = {};
-          var val = obj[property][i];
-          var propKey = parentKey + '.' + property + '.' + i;
-
-          if (_.isString(val)) {
-            propObj[propKey] = processString(val);
-          } else {
-            // FIXME There is only so much that can be done here
-            propObj[propKey] = JSON.stringify(val);
-          }
-
-          processedObject.push(propObj);
-        }
-      } else {
-        console.log('unsupported data type, dude that blows. I don"t even know how we go there?');
-      }
-    }
-  }
-}
-
-
-function processObject(obj) {
-
-  var processedObject = [];
-
-  for (var k in obj) {
-    if (obj.hasOwnProperty(k)) {
-      flatten(obj[k], k, processedObject);
-    }
-  }
-
-  return processedObject;
-}
 
 module.exports = Writer;
